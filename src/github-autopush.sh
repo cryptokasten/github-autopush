a2beddde2e8f160e63bf0fad13af05d446995be0
Cryptokasten <crypto@mail.com>
2020-06-08 14:34:50 +0300

    Github autopush.

---
+++ src/github-autopush.sh
@@ -0,0 +1,11 @@
+source ~/.github
+
+#!/bin/bash
+
+ORG_NAME="cryptokasten"
+
+PROJECT_NAME=`pwd | rev | cut -d"/" -f 1 | rev`
+
+curl -H "Authorization: token $GITHUB_TOKEN" --data "{\"name\":\"$PROJECT_NAME\"}" https://api.github.com/orgs/$ORG_NAME/repos
+
+git remote add origin git@github.com:$ORG_NAME/$PROJECT_NAME.git
