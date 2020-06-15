4ec207bcacbdfece5dc93bac2612122b3163da62
Cryptokasten <crypto@mail.com>
2020-06-15 15:31:38 +0300

    Added push command.

--- src/github-autopush.sh
+++ src/github-autopush.sh
@@ -9,3 +9,5 @@ PROJECT_NAME=`pwd | rev | cut -d"/" -f 1 | rev`
 curl -H "Authorization: token $GITHUB_TOKEN" --data "{\"name\":\"$PROJECT_NAME\"}" https://api.github.com/orgs/$ORG_NAME/repos
 
 git remote add origin git@github.com:$ORG_NAME/$PROJECT_NAME.git
+
+git push
